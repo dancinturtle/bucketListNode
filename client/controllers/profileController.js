5bea9f96bc252175437585901fc78da37567ae3f
dancinturtle <amy.giver@gmail.com>
2016-06-25 23:39:41 -0700

    Is it done?

--- client/controllers/profileController.js
+++ client/controllers/profileController.js
@@ -1,11 +1,7 @@
 beltExam.controller('ProfileController', function($scope, BucketFactory, UserFactory,  $routeParams, $location){
   console.log("Route params!!!!!", $routeParams);
 
-  $scope.othercompletedBuckets = [];
 
-  $scope.otherpendingBuckets = [];
-  $scope.ourcompletedBuckets = [];
-  $scope.ourpendingBuckets = [];
 
   UserFactory.shareUser(function(data){
     $scope.currentUser = data;
@@ -15,25 +11,41 @@ beltExam.controller('ProfileController', function($scope, BucketFactory, UserFac
     $scope.viewedUser = $scope.viewedUser[0];
     console.log("THE user to VIEW", $scope.viewedUser);
   });
-  //
-  BucketFactory.userBuckets($routeParams.name, function(data){
-    $scope.buckets = data;
-    console.log("USER BUCKETS", $scope.buckets);
-    for(var i=0; i<$scope.buckets.length; i++){
-      if($scope.buckets[i].completed == true && $scope.buckets[i].createdBy == $scope.currentUser.name || $scope.buckets[i].completed == true && $scope.buckets[i].createdFor == $scope.currentUser.name){
-        $scope.ourcompletedBuckets.push($scope.buckets[i]);
+
+  $scope.getUserBuckets = function(){
+    $scope.othercompletedBuckets = [];
+    $scope.otherpendingBuckets = [];
+    $scope.ourcompletedBuckets = [];
+    $scope.ourpendingBuckets = [];
+    BucketFactory.userBuckets($routeParams.name, function(data){
+      $scope.buckets = data;
+      console.log("USER BUCKETS", $scope.buckets);
+      for(var i=0; i<$scope.buckets.length; i++){
+        if(($scope.buckets[i].completed == true && $scope.buckets[i].createdBy == $scope.currentUser.name) || ($scope.buckets[i].completed == true && $scope.buckets[i].createdFor == $scope.currentUser.name)){
+          $scope.ourcompletedBuckets.push($scope.buckets[i]);
+        }
+        else if($scope.buckets[i].completed == true && $scope.buckets[i].createdBy != $scope.currentUser.name && $scope.buckets[i].createdFor != $scope.currentUser.name){
+          $scope.othercompletedBuckets.push($scope.buckets[i]);
+        }
+        else if(($scope.buckets[i].completed == false && $scope.buckets[i].createdBy == $scope.currentUser.name) || ($scope.buckets[i].completed == false && $scope.buckets[i].createdFor == $scope.currentUser.name)){
+          $scope.ourpendingBuckets.push($scope.buckets[i]);
+        }
+        else {
+          $scope.otherpendingBuckets.push($scope.buckets[i]);
+        }
       }
-      else if($scope.buckets[i].completed == true && $scope.buckets[i].createdBy!=$scope.currentUser.name&&$scope.buckets[i].createdFor!=$scope.currentUser.name){
-        $scope.othercompletedBuckets.push($scope.buckets[i]);
-      }
-      else if($scope.buckets[i].completed == false && $scope.buckets[i].createdBy == $scope.currentUser.name || $scope.buckets[i].completed == true && $scope.buckets[i].createdFor == $scope.currentUser.name){
-        $scope.ourpendingBuckets.push($scope.buckets[i]);
-      }
-      else {
-        $scope.otherpendingBuckets.push($scope.buckets[i]);
-      }
-    }
-  })
+    })
+  }
+  $scope.getUserBuckets();
+
+  $scope.changeStatus = function(id, status){
+    var updatingBucket = {id: id, status: status};
+    console.log("Changing the status", updatingBucket);
+    BucketFactory.updateBucket(updatingBucket, function(data){
+      console.log(data);
+    })
+    $scope.getUserBuckets();
+  }//closes changeStatus
 
 
 })//closes profileController
--- client/factories/userFactory.js
+++ client/factories/userFactory.js
@@ -1,13 +1,14 @@
 beltExam.factory('UserFactory', function($http){
   var factory = {};
-  var user;
+  var currentuser;
+  var profileuser;
   var users = [];
 
   factory.findUserforProfile = function(name, callback){
     console.log("Factory", name)
     $http.get('/findUserforProfile/'+name).success(function(res){
-      user = res;
-      console.log("Our current user", user);
+      profileuser = res;
+      console.log("Our current user", profileuser);
       callback(res);
     })
   }//closes index
@@ -15,8 +16,8 @@ beltExam.factory('UserFactory', function($http){
   factory.findUser = function(name, callback){
     console.log("Factory", name)
     $http.get('/findUser/'+name).success(function(res){
-      user = res;
-      console.log("Our current user", user);
+      currentuser = res;
+      console.log("Our current user", currentuser);
       callback(res);
     })
   }//closes index
@@ -27,7 +28,7 @@ beltExam.factory('UserFactory', function($http){
       })
     }
   factory.shareUser = function(callback){
-    callback(user);
+    callback(currentuser);
   }
 
   factory.index = function(callback){
@@ -36,18 +37,6 @@ beltExam.factory('UserFactory', function($http){
       callback(users);
     })
   }//closes index
-//   factory.delete = function(id, callback){
-//
-//     $http.delete('/customers/'+id).success(function(res){
-//       callback(res.message);
-//     })
-//   }
-//   factory.create = function(newcustomer, callback){
-//     $http.post('/addcustomers', newcustomer).success(function(res){
-//       callback(res.message);
-//     })
-//   }
-// //
-// //
+
   return factory;
 })//closes CustomerFactory
